87caeb4e8bcbc52903925347ba37abb5cd83552d
dina2803 <dg734939@gmail.com>
2023-10-25 14:25:02 +0600

    dz4_dina_29_01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    phoneNumber: '',
+    email: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div className="registration-form">
+          <h2>Register</h2>
+          <form>
+            <div className="form-group">
+              <label htmlFor="fullName">First Name and Last Name</label>
+              <input
+                  type="text"
+                  id="fullName"
+                  name="fullName"
+                  value={formData.fullName}
+                  onChange={handleChange}
+                  placeholder="Enter your name..."
+                />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phoneNumber">Phone number</label>
+              <input
+                  type="tel"
+                  id="phoneNumber"
+                  name="phoneNumber"
+                  value={formData.phoneNumber}
+                  onChange={handleChange}
+                  placeholder="+996-700-700-700"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Enter your email.."
+              />
+            </div>
+            <div className="preview-data">
+              <h3>Your information:</h3>
+              <p>First Name and Last Name: {formData.fullName}</p>
+              <p>Phone number: {formData.phoneNumber}</p>
+              <p>Email: {formData.email}</p>
+            </div>
+          </form>
+        </div>
+      </div>
   );
 }
 
 export default App;
+
